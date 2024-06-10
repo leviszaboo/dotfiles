329eaadc5592b694cd48132f05b3e276687d02b4
Levi <leviszaboo@icloud.com>
2024-06-10 11:53:22 +0200

    minor changes

--- .config/bottom_bar/colors.sh
+++ .config/bottom_bar/colors.sh
@@ -3,8 +3,8 @@
 export WHITE=0xffdcd7ba
 
 # export BAR_COLOR=0x00000000
-export BAR_COLOR=0xff363646
-export BRACKET_COLOR=0xff363646
+export BAR_COLOR=0xff2a2a37
+export BRACKET_COLOR=0xff1e1f28
 export ACCENT_COLOR=0xffff9e3b
 export DARK_BG=0xff1a1a22
 export SECONDARY_COLOR=0xff9cabca
--- .config/sketchybar/items/filler.sh
+++ .config/sketchybar/items/filler.sh
@@ -4,4 +4,4 @@ sketchybar --add item filler.left left \
            --set filler.left padding_left=520
 
 sketchybar --add item filler.right right \
-           --set filler.right padding_right=215
+           --set filler.right padding_right=210
